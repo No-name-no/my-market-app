8d271dd6955d5bdbb2dd166b51a4fb8d12858906
mnuykin <n_mish@mail.ru>
2026-09-19 08:35:46 +0500

    Спринт 8 - тесты с учетом пользователей

--- market-app/src/test/java/org/mnuykin/mymarket/service/OrderServiceTest.java
+++ market-app/src/test/java/org/mnuykin/mymarket/service/OrderServiceTest.java
@@ -8,6 +8,7 @@ import org.mnuykin.mymarket.model.ItemDto;
 import org.mnuykin.mymarket.model.OrderDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.test.context.support.WithMockUser;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
@@ -29,6 +30,7 @@ class OrderServiceTest extends BaseServiceTest{
     private PaymentApi paymentApi;
 
     @Test
+    @WithMockUser(username = "test", password = "{bcrypt}$2a$10$jJt7W4VW0300oj.Lqon17uQpg7jL7qxjRQZvd/5VjR9fwFBzc5dI6")
     void buy(){
         ExecuteResponse response = new ExecuteResponse();
         response.setStatus(ExecuteResponse.StatusEnum.SUCCESSFUL);
